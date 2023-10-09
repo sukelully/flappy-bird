9011e268170f249397dc70eefcaa133d6aa76415
Luke Sullivan <sully097@gmail.com>
2023-10-09 17:24:23 +0100

    Initial commit

---
+++ src/main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include "SFML/Graphics.hpp"
+
+
+int main(int argc, const char * argv[]) {
+    sf::RenderWindow window (sf::VideoMode(800, 600), "Flappy Bird");
+    
+    return 0;
+}
